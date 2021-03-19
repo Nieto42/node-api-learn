11b81e66d1d9de795e04e9784c1591dc2ed35720
Neto Pompeu <neto1@hotmail.fr>
2021-03-19 15:20:46 +0100

    start project

---
+++ models/dbConfig.js
@@ -0,0 +1,10 @@
+const moongoose = require('mongoose')
+
+moongoose.connect(
+    "mongodb://localhost:27017/node-api",
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err) => {
+        if(!err) console.log("Mongodb connected");
+        else console.log("Connection error :" + err);
+    }
+)
